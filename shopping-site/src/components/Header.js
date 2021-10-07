9ea58d41f1e0089e5c6885984624b0e4c010f262
Damian Thompasionas <dthompasionas@gmail.com>
2021-10-07 19:36:50 -0400

    initial commit

---
+++ shopping-site/src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { CarouselItem, Container, Dropdown, FormControl, Navbar, Nav, Badge } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <a href="/">Shopping Cart</a>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            placeholder="Search for product"
+            className="m-auto"
+          />
+        </Navbar.Text>
+        <Nav>
+            <Dropdown alignRight>
+                <Dropdown.Toggle variant = 'success' >
+                    {/* <FaShoppingCart color='white' fontSize='25px' /> */}
+                    <Badge>{10}</Badge> 
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu style={{ minWidth: 370 }}>
+                    <span style={{ padding: 10 }}>Cart is Empty!</span>
+                </Dropdown.Menu>
+            </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
