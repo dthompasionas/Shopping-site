6f91d86ed58370213e3be16aad58fe470b74010c
Damian Thompasionas <dthompasionas@gmail.com>
2021-10-07 23:45:19 -0400

    added add to cart functionality and cart page

--- shopping-site/src/components/Header.js
+++ shopping-site/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import {FaShoppingCart} from 'react-icons/fa';
-import { Container, Dropdown, FormControl, Navbar, Nav, Badge } from "react-bootstrap";
+import { Container, Dropdown, FormControl, Navbar, Nav, Badge, Button } from "react-bootstrap";
 import { CartState } from "../context/Context";
 import { AiFillDelete } from "react-icons/ai";
+import Rating from "./Rating";
 
 const Header = () => {
 
@@ -13,7 +14,7 @@ const Header = () => {
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
-          <Link href="/">Shopping Cart</Link>
+          <Link to="/">Shopping Cart</Link>
         </Navbar.Brand>
         <Navbar.Text className="search">
           <FormControl
@@ -39,6 +40,7 @@ const Header = () => {
                           <div className='cartItemDetail'>
                             <span>{prod.name}</span>
                             <span>$ {prod.price.split(".")[0]}</span>
+                            {/* <Rating rating={prod.ratings}/> */}
                           </div>
                           <AiFillDelete fontSize='20px' style={{cursor: 'pointer'}} onClick={() => dispatch({
                             type: 'REMOVE_FROM_CART',
@@ -47,6 +49,11 @@ const Header = () => {
 
                         </span>
                       ))}
+                      <Link to='/cart'>
+                        <Button style={{ width: '95%', margin: '0 10px'}}>
+                          Go to Cart
+                        </Button>
+                      </Link>
                     </>
 
                   ) : (
