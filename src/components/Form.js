12fb3f7cd884d19d7b5d5eed0af3496dfbc3f4b0
Damian Thompasionas <dthompasionas@gmail.com>
2021-10-13 01:49:57 -0400

    added checkout functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,9 +6,10 @@ const CheckoutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
 
+
     async function handleSubmit(e) {
         e.preventDefault()
-
+        
         const { error, paymentMethod } = await stripe.createPaymentMethod(
             {
                 type: 'card',
@@ -18,21 +19,36 @@ const CheckoutForm = () => {
 
         if(error) {
             console.log(error);
+
+            document.getElementById('thank-you').innerHTML= '';
+            document.getElementById('valid-card').innerHTML= 'Please enter valid card information*';
+            
+
+            
+
         } else {
             console.log(paymentMethod);
+            
+            document.getElementById('valid-card').innerHTML = '';
+            document.getElementById('thank-you').innerHTML = 'Thank you for your purchase!';
         }
+        
     }
+
+
 
     return (
         <form onSubmit={handleSubmit}>
             <div className='card-input'>
                 <CardElement/>
             </div>
+            <p id='valid-card'></p>
             <div className='pay-btn'>
                 <Button type='submit' disabled= {!stripe} onClick={handleSubmit}>
                     Pay Now
                 </Button>
             </div>
+            <p id='thank-you'></p>
             
         </form>
     )
